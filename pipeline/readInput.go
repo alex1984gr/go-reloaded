3cf9e6a3df843c23a2c883837b9081b0eb14dd00
agaliand <alexandrosgaliandras@yahoo.com>
2025-11-01 22:51:30 +0200

    readInput.go

--- pipeline/readInput.go
+++ pipeline/readInput.go
@@ -1,15 +1,13 @@
 package pipeline
 
 import (
-	"bufio"
 	"os"
 )
 
-func ReadInput(s string) []rune {
-	scanner := bufio.NewScanner(os.Stdin) // δημιουργεί ένα σκάνερ που διαβάζει γραμμές από το τερματικό
-	text := ""                            // δημιουργεί μεταβλητή text τύπου string για αποθήκευση κειμένου
-	for scanner.Scan() {                  // loop που τρέχει όσο υπάρχουν γραμμές για να διαβάσει ο scanner
-		text += scanner.Text() + " " // προσθέτει τη γραμμή που διάβασε το scanner στο string text
+func ReadInput(path string) (string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
 	}
-	return []rune(text) // μετατρέπει όλο το string σε slice από runes
+	return string(data), nil
 }
