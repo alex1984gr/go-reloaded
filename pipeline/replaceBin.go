2b20deebd95a2dde88cf05693ee82c58f9270cbd
agaliand <alexandrosgaliandras@yahoo.com>
2025-11-09 00:33:13 +0200

    replaceBin.go

--- pipeline/replaceBin.go
+++ pipeline/replaceBin.go
@@ -5,12 +5,16 @@ import (
 	"strconv"
 )
 
-func ReplaceBin(input string) string {
+func ReplaceBin(input []string) []string {
 	re := regexp.MustCompile(`\b[01]+\b`)
-	return re.ReplaceAllStringFunc(input, func(bin string) string {
-		if val, err := strconv.ParseInt(bin, 2, 64); err == nil {
-			return strconv.FormatInt(val, 10)
-		}
-		return bin
-	})
+
+	for i, word := range input {
+		input[i] = re.ReplaceAllStringFunc(word, func(bin string) string {
+			if val, err := strconv.ParseInt(bin, 2, 64); err == nil {
+				return strconv.FormatInt(val, 10)
+			}
+			return bin
+		})
+	}
+	return input
 }
