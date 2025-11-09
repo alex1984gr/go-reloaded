70dcd9e8b171e8d934cdaddf04574245561a7de7
agaliand <alexandrosgaliandras@yahoo.com>
2025-11-10 00:07:11 +0200

    tokenize.go

--- pipeline/tokenize.go
+++ pipeline/tokenize.go
@@ -1,26 +1,26 @@
 package pipeline
 
 func Tokenize(input []rune) []string {
-	tokens := []string{} // slice για να αποθηκεύσει τα tokens
-	current := ""        // προσωρινή μεταβλητή για την τρέχουσα λέξη
+	tokens := []string{} // slice to store the tokens
+	current := ""        // temporary variable to hold the current word
 
-	for _, ch := range input { // loop σε κάθε χαρακτήρα του input
-		if ch == '<' || ch == '>' || ch == '"' || ch == ' ' { // αν είναι ειδικός χαρακτήρας
-			if current != "" { // αν υπάρχει κάτι στο current
-				tokens = append(tokens, current) // προσθέτει την τρέχουσα λέξη στα tokens
-				current = ""                     // καθαρίζει το current
+	for _, ch := range input { // loop through each character in the input
+		if ch == '<' || ch == '>' || ch == '"' || ch == ' ' { // if it’s a special character
+			if current != "" { // if there’s something stored in 'current'
+				tokens = append(tokens, current) // add the current word to the tokens
+				current = ""                     // clear 'current'
 			}
-			if ch != ' ' { // αν δεν είναι κενό
-				tokens = append(tokens, string(ch)) // προσθέτει τον ειδικό χαρακτήρα στα tokens
+			if ch != ' ' { // if it’s not a space
+				tokens = append(tokens, string(ch)) // add the special character as a token
 			}
 		} else {
-			current += string(ch) // αλλιώς προσθέτει τον χαρακτήρα στο current
+			current += string(ch) // otherwise, add the character to 'current'
 		}
 	}
 
-	if current != "" { // αν υπάρχει υπόλοιπο στο current
-		tokens = append(tokens, current) // το προσθέτει στα tokens
+	if current != "" { // if there’s anything left in 'current'
+		tokens = append(tokens, current) // add it to the tokens
 	}
 
-	return tokens // επιστρέφει όλα τα tokens
+	return tokens // return all tokens
 }
