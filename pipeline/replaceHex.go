d17bd9ebb2e8747062ae34f1c1f3782fe38fa21e
agaliand <alexandrosgaliandras@yahoo.com>
2025-11-09 02:46:10 +0200

    replaceHex.go

--- pipeline/replaceHex.go
+++ pipeline/replaceHex.go
@@ -7,16 +7,22 @@ import (
 )
 
 func ReplaceHex(lines []string) []string {
-	re := regexp.MustCompile(`\b[0-9A-Fa-f]+\b`)
+	re := regexp.MustCompile(`\b([0-9A-Fa-f]+)\s*\(hex\)`)
 
 	var result []string
 
 	for _, line := range lines {
-		converted := re.ReplaceAllStringFunc(line, func(hexStr string) string {
-			decValue, err := strconv.ParseInt(hexStr, 16, 64)
-			if err != nil {
-				return hexStr
+		converted := re.ReplaceAllStringFunc(line, func(match string) string {
+			hexMatch := regexp.MustCompile(`^[0-9A-Fa-f]+`).FindString(match)
+			if hexMatch == "" {
+				return match
 			}
+
+			decValue, err := strconv.ParseInt(hexMatch, 16, 64)
+			if err != nil {
+				return match
+			}
+
 			return fmt.Sprintf("%d", decValue)
 		})
 		result = append(result, converted)
